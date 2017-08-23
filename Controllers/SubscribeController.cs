760c76b36579e2fb169e89e014fefa07f9ef90c8
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-23 14:22:45 -0400

    emails.csv works. now to display the mailing list.

--- Controllers/SubscribeController.cs
+++ Controllers/SubscribeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +11,20 @@ namespace WelcomeToWeb.Controllers
 {
     public class SubscribeController : Controller
     {
-        [Route("/Subscribe")]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index(string email)
+        public IActionResult Index(string email, string name)
         {
+            ViewData["name"] = name;
             ViewData["email"] = email;
+            using (var writer = new StreamWriter(System.IO.File.Open("emails.csv", FileMode.Append)))
+            {
+                writer.WriteLine($"{name} | {email}");
+            }
+
             return View();
         }
         public IActionResult Error()
